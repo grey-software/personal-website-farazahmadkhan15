cdac9084b14bb2dfeaa0e45e40a7bfcf89621dd0
farazahmadkhan15 <farazahmadkhan15@gmail.com>
2021-03-15 23:44:32 +0500

    Completed Hero Componet

--- components/Hero.vue
+++ components/Hero.vue
@@ -7,10 +7,11 @@
 <div class="flex-col text h-full">
   <NavBar/>
  <div class="container mx-auto flex px-5 py-8 items-center justify-center flex-col">
-    <img class="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover p-0 object-center rounded" alt="hero" src="https://dummyimage.com/720x600">
+    <img class="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover p-0 object-center rounded-full" alt="hero" src="~/assets/faraz.jpg">
     <div class="text-center lg:w-2/3 w-full">
-      <h1 class=" sm:text-4xl text-3xl mb-4 p-1 font-medium font-fans italic text-gray-400">Faraz Ahmad Khan</h1>
-      <p class="mb-8 leading-relaxed">Meggings kinfolk echo park stumptown DIY, kale chips beard jianbing tousled. Chambray dreamcatcher trust fund, kitsch vice godard disrupt ramps hexagon mustache umami snackwave tilde chillwave ugh. Pour-over meditation PBR&amp;B pickled ennui celiac mlkshk freegan photo booth af fingerstache pitchfork.</p>
+      <h1 class=" sm:text-4xl text-3xl mb-4 p-1 font-extrabold font-fans italic text-gray-400">Faraz Ahmad Khan</h1>
+      <p class="mb-8 font-fans font-bold font-xl italic leading-relaxed">Hello! I am Faraz Ahmad Khan, a Software Engineer with the passion for developing 
+        innovative programs.I love to work on Website, REST APIs and Machine Learning. I always enjoy to learn new languages and development techniques.  </p>
 
     </div>
   </div>
@@ -62,7 +63,7 @@ canvas {
 	// color: $text;
 	max-width: 90%;
 	// padding: 2em 3em;
-	background: rgba(0, 0, 0, 0.4);
+	// background: rgba(0, 0, 0, 0.4);
 	text-shadow: 0px 0px 2px #131415;
 	font-family: 'Open Sans', sans-serif;
 }

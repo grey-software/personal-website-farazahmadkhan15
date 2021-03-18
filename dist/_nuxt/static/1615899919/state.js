91bffb668775b199e816ac7614b73a4b341621a3
farazahmadkhan15 <farazahmadkhan15@gmail.com>
2021-03-18 21:18:42 +0500

    deployment

--- dist/_nuxt/static/1615899919/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1615899919",layout:"default",error:a,serverRendered:true,routePath:b,config:{app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"73739c9c"}}}}(null,"\u002F"));
